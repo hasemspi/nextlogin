e164cf1f4a935f80089762ece2b4a5f523c8b707
hasemspi <hasemspi@gmail.com>
2025-11-11 09:40:51 +0600

    login page update

--- src/app/componets/login/page.js
+++ src/app/componets/login/page.js
@@ -2,8 +2,17 @@
 
 const LoginPage = () => {
     return (
-        <div>
-            
+        <div className="mx-auto">
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+              
+                <label className="label">Email</label>
+                <input type="email" className="input" placeholder="Email" />
+
+                <label className="label">Password</label>
+                <input type="password" className="input" placeholder="Password" />
+
+                <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
         </div>
     );
 };
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,13 +1,20 @@
 import Footersection from "../componets/footer/page";
+import LoginPage from "../componets/login/page";
 import Navber from "../componets/navber/page";
 
 
 const LoginForm = () => {
     return (
-        <div className="container mx-auto">
-            <Navber/>
-            <h1>Hello Login</h1>
-            <Footersection/>
+        <div>
+            <Navber />
+            <div className="card bg-base-100 w-96 shadow-sm">
+                <div className="card-body">
+                    <h2 className="card-title">User Loign</h2>
+                    <LoginPage />
+                </div>
+            </div>
+
+            <Footersection />
         </div>
     );
 };
